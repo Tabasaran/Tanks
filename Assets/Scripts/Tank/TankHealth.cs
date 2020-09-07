cb96042f1b5527e9070b7dfe534e56a83b69b48f
Tabasaran <1tabasaran@gmail.com>
2020-09-07 13:10:09 +0300

    Creating and Firing Shells

--- Assets/Scripts/Tank/TankHealth.cs
+++ Assets/Scripts/Tank/TankHealth.cs
@@ -65,7 +65,9 @@ public class TankHealth : MonoBehaviour
         // Play the effects for the death of the tank and deactivate it.
         m_Dead = true;
 
+        m_ExplosionParticles.transform.position = transform.position;
         m_ExplosionParticles.gameObject.SetActive(true);
+        
         m_ExplosionParticles.Play();
 
         m_ExplosionAudio.Play();
